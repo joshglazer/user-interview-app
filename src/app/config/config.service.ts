0f48798cac9f1f07205bafb2553cda58148c5a83
jackmasterflex <jlopez1992@yahoo.com>
2019-03-15 14:16:43 -0400

    forgotten code

--- src/app/config/config.service.ts
+++ src/app/config/config.service.ts
@@ -10,25 +10,11 @@ export class ConfigService {
     constructor(private http: HttpClient) { }
     
     private handleError(error: HttpErrorResponse) {
-        if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error('An error occurred:', error.error.message);
-        } else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
-        }
-        
-        // return an observable with a user-facing error message
-        return throwError('Something bad happened; please try again later.');
+        // error handler here
     };
 
     getConfig(externalLink) {
-        return this.http.get(externalLink)
-         .pipe(
-            retry(3), // retry a failed request up to 3 times
-            catchError(this.handleError) // then handle the error
-        );
+        // call api here
     }
     
 }
